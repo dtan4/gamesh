8d5a0a0a71ef5696f95969a7abef89f5b731f2e2
Daisuke Fujita <dtanshi45@gmail.com>
2014-12-14 15:18:44 +0900

    Show indices

--- commands.go
+++ commands.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/codegangsta/cli"
 )
@@ -44,4 +48,29 @@ func doGet(c *cli.Context) {
 }
 
 func doList(c *cli.Context) {
+	resp, err := http.Get("http://tokyo-ame.jwa.or.jp/scripts/mesh_index.js")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	indexList := strings.Replace(string(body), "Amesh.setIndexList(", "", -1)
+	indexList = strings.Replace(indexList, ");", "", -1)
+	indexList = strings.Replace(indexList, "\n", "", -1)
+	indexList = strings.Replace(indexList, "\"", "", -1)
+	indexList = strings.Replace(indexList, "[", "", -1)
+	indexList = strings.Replace(indexList, "]", "", -1)
+	indexList = strings.Replace(indexList, ",", "\n", -1)
+
+	fmt.Println(indexList)
 }
